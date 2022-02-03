21b88f6f40c62f278280f13f7b253eac6f83c17e
magavk <magavk@gmail.com>
2022-02-03 16:52:49 +0200

    secondCommit

--- homeWorkLesson3/homeworkLesson3/Program.cs
+++ homeWorkLesson3/homeworkLesson3/Program.cs
@@ -6,7 +6,7 @@ namespace homeworkLesson3
     {
         static void Main(string[] args)
         {
-            int x = 2;
+            byte x = 2;
             int y = 3;
             Console.WriteLine($"-6 * x ^ 3 + 5 * x ^ 2 - 10 * x + 15 = {-6 * Math.Pow(x, 2) + 5 * Math.Pow(x, 2) - 10 * x + 15}");
             Console.WriteLine($"abs(x) * sin(x) = { Math.Abs(x) * Math.Sin(x)}");
@@ -19,10 +19,6 @@ namespace homeworkLesson3
             Console.WriteLine($"{today}");
             Console.WriteLine($"{(today-pastNY).ToString("%d")} days passed from New Year");
             Console.WriteLine($"{(futureNY - today).ToString("%d")} days left to New Year");
-
-            var i = 5;
-
-            Console.WriteLine(i++);
         }
     }
 }
